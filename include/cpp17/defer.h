57dbb3ebccf197b8858f4a89f9f38341382642c9
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2023-11-22 12:24:18 +0000

    Tweak to defer

--- include/cpp17/defer.h
+++ include/cpp17/defer.h
@@ -11,45 +11,42 @@
 namespace dp {
 
 	template<typename Callable, std::enable_if_t<std::is_invocable_v<Callable>, bool> = true>
-	class Defer {
+	class defer {
 
-#ifndef DP_DEFER_ALLOW_NON_NOEXCEPT
-		//For error handling purposes we hard-require that a deferred statement be noexcept
-		static_assert(noexcept(Callable), "Deferred callables must be noexcept");
-#endif
 
 		//Filter out function types down to function pointers
 		using defer_type = std::decay_t<Callable>;
 
 		defer_type m_call;
 
+
 	public:
 
 		template<typename T = Callable, std::enable_if_t<std::is_convertible_v<T, Callable>, bool> = true>
-		constexpr Defer(T&& in) : m_call{ std::forward<T>(in) } {}
+		constexpr defer(T&& in) : m_call{ std::forward<T>(in) } {}
 
 		//By definition this is a scope-local, one-and-done tool. We don't want multiple copies of the same deferral
-		Defer() = delete;
-		Defer(const Defer&) = delete;
-		Defer(Defer&&) = delete;
+		defer() = delete;
+		defer(const defer&) = delete;
+		defer(defer&&) = delete;
 
-		Defer& operator=(const Defer&) = delete;
-		Defer& operator=(Defer&&) = delete;
+		defer& operator=(const defer&) = delete;
+		defer& operator=(defer&&) = delete;
 
 
-		~Defer() noexcept {
+		~defer() noexcept {
 			std::invoke(m_call);
 		}
 	};
 	template<typename T>
-	Defer(T) -> Defer<T>;
+	defer(T) -> defer<T>;
 
 }
 
 /*
-*   In the macro case, we want to be able to generate implicit Defer instances with unique names.
+*   In the macro case, we want to be able to generate implicit defer instances with unique names.
 *   As such we use __COUNTER__ if it's a available and __LINE__ if not, and concat each one such that
-*   the macro generates classes called Defer_Struct1, Defer_Struct2, Defer_Struct3. Unique but still
+*   the macro generates classes called defer_Struct1, defer_Struct2, defer_Struct3. Unique but still
 *   understandable and diagnosable if needed.
 */
 #define DEFER_CONCAT_IMPL(x,y) x##y
@@ -64,7 +61,7 @@ namespace dp {
 //Note that a [&] isn't the sledgehammer it may appear to be.
 //A lambda is not required to actually capture variables it does not use, and since this is a scope-local construct we are unlikely to run into scope issues
 //Varidadic macro to prevent commas being problematic
-#define DEFER(...) [[maybe_unused]] auto DEFER_CONCAT_MACRO(Defer_Struct, DEFER_COUNT) = dp::Defer([&]() noexcept { __VA_ARGS__ ;});
+#define DEFER(...) [[maybe_unused]] auto DEFER_CONCAT_MACRO(defer_Struct, DEFER_COUNT) = dp::defer([&]() noexcept { __VA_ARGS__ ;});
 
 
 
